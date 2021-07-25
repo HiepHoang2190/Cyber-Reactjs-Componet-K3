41c7b0a167436bbf7a77dea266220e357424488d
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-25 15:26:46 +0700

    ReactJs-K3: Lesson 6: styled component demo đầu tiên

--- react-component-k3/src/App.js
+++ react-component-k3/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UserProfile from './FormValidation/UserProfile/UserProfile';
+import DemoJSS from './JSS_StyledComponent/DemoJSS/DemoJSS';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <UserProfile />
+        {/* <UserProfile /> */}
+        <DemoJSS />
       </div>
     );
   }
---
+++ react-component-k3/src/JSS_StyledComponent/DemoJSS/DemoJSS.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from '../Components/Button';
+
+class DemoJSS extends Component {
+    render() {
+        return (
+            <div>
+                <Button className="button_style" >Hello Hiệp</Button>
+            </div>
+        );
+    }
+}
+
+export default DemoJSS;
