f013f1dd8187f937fd566327827fcc183f4efda4
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-25 08:03:06 +0700

    ReactJs-K3: lesson 1:Giới thiệu về khóa học, kiến thức form và validation qua ví dụ

---
+++ react-component-k3/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserProfile from './FormValidation/UserProfile/UserProfile';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserProfile />
+      </div>
+    );
+  }
+}
+
+export default App;
