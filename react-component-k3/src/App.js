96f66e8d8d33fd2875f2a3f2747799396e47658b
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-31 11:20:14 +0700

    ReactJs-K3: Lesson 19:Khái niệm react lifecycle

--- react-component-k3/src/App.js
+++ react-component-k3/src/App.js
@@ -3,6 +3,7 @@ import UserProfile from './FormValidation/UserProfile/UserProfile';
 import ToDoList from './JSS_StyledComponent/BaiTapStyledComponent/ToDoList/ToDoList';
 import DemoJSS from './JSS_StyledComponent/DemoJSS/DemoJSS';
 import DemoTheme from './JSS_StyledComponent/Theme/DemoTheme';
+import LifeCycleReact from './LifeCycleReact/LifeCycleReact';
 
 
 
@@ -13,7 +14,8 @@ class App extends Component {
         {/* <UserProfile /> */}
         {/* <DemoJSS /> */}
         {/* <DemoTheme /> */}
-        <ToDoList />
+        {/* <ToDoList /> */}
+        <LifeCycleReact />
       </div>
     );
   }
